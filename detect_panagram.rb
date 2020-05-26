a8ef1d6ee0e67c4675b3651832335abf29d840e0
JordonGoodsir <jordon.goodsir1@gmail.com>
2020-05-27 09:47:28 +1000

    challenge complete!

---
+++ detect_panagram.rb
@@ -0,0 +1,31 @@
+# Challenge: 
+# A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
+
+# Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
+
+def panagram?(string)
+     
+    
+    #array a-z  
+     
+    #split string, do .uniq 
+    
+    #reject all punctuation and spaces 
+
+    #compare with a-z  
+    
+    alphabet = [* "a".."z"] 
+
+   
+    if alphabet == string.downcase.gsub(/[^a-z""]/, "").split(//).uniq.sort 
+        return true 
+    else 
+        return false 
+    end
+  
+ 
+
+end 
+
+ 
+panagram?("The quick brown fox jumps over the lazy dog.")
